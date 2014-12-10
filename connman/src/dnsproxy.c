908a37aae1bbada373ef2d518cc659e0be1ce2af
Jarko Poutiainen <Jarko.Poutiainen@oss.tieto.com>
2014-12-10 08:44:14 +0200

    [connman] dnsproxy: fetch inet6 ifname if AF_INET6
    Signed-off-by: Jarko Poutiainen <Jarko.Poutiainen@oss.tieto.com>

--- connman/src/dnsproxy.c
+++ connman/src/dnsproxy.c
@@ -2465,7 +2465,11 @@ static int server_create_socket(struct server_data *data)
 
 	DBG("sk %d", sk);
 
-	interface = connman_inet_ifname(data->index);
+	if (data->server_addr->sa_family == AF_INET6)
+		interface = connman_inet6_ifname(data->index);
+	else
+		interface = connman_inet_ifname(data->index);
+
 	if (interface) {
 		if (setsockopt(sk, SOL_SOCKET, SO_BINDTODEVICE,
 					interface,
